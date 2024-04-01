962a81d6d0ad49ece3b647706f4cbd7eb1916279
vasu <vasu.gupta9@gmail.com>
2024-04-01 13:23:25 +0530

    working version 1

--- main.py
+++ main.py
@@ -4,28 +4,54 @@ from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
 import os
 
-client = OpenAI()
+def find_answer(input_context, question, openai_api_key):
+    # set client
+    client = OpenAI(api_key=openai_api_key)
 
-# response = client.chat.completions.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {"role": "system", "content": "You are a helpful assistant."},
-#     {"role": "user", "content": "Who won the world series in 2020?"},
-#     {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-#     {"role": "user", "content": "Where was it played?"}
-#   ]
-# )
+    # Define conversion prompt
+    prompt = f"Answer the following question based on provided context.\n\nQuestion - ```{question}```\n\nContext - ```{input_context}```"
+    print("Prompt:")
+    print(prompt)
+    print("="*100)
 
+    # find/generate answer using LLM
+    response = client.chat.completions.create(
+        model="gpt-4-turbo-preview",
+        messages=[
+            { "role": "user", "content": prompt}
+        ]
+    )
+    print('response\n')
+    print(response)
+    
+    # extract answer 
+    answer = response.choices[0].message.content.strip()
+    return answer
 
 # add openai_api_key to environment variables
 # While you can provide an api_key keyword argument, using python-dotenv may be preferred so that your API Key is not stored in source control
 # steps - create .env file and add api key as OPENAI_API_KEY="My API Key"
 _ = load_dotenv(find_dotenv()) # finds .env file and adds key-value pairs specified in .env to enviornment variables
 openai_api_key   = os.environ.get("OPENAI_API_KEY") 
-print("openai_api_key: ", openai_api_key)
+#print("openai_api_key: ", openai_api_key)
 if (openai_api_key is None) :
     print('OpenAI API key not found. Please check .env file exists with key "OPENAI_API_KEY" present in file')
     exit()
 if (len(openai_api_key.strip()) == 0) :
     print('Missing OpenAI API key - please provide api key value in .env file as OPENAI_API_KEY="My API Key"')
     exit()
+
+# define parameters 
+context_filepath  = './samples/opencv_live_episode_123/context.txt' # shortened_context.txt
+question_filepath = './samples/opencv_live_episode_123/question.txt'   
+
+# read context 
+with open(context_filepath) as fh : 
+    input_context = fh.read()
+
+# read question
+with open(question_filepath) as fh : 
+    question = fh.read()
+
+answer = find_answer(input_context, question, openai_api_key)
+print("answer:", answer)
---
+++ token_counts.py
@@ -0,0 +1,21 @@
+import tiktoken 
+
+# encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+# print('encoding for gpt-3.5-turbo:', encoding) # prints <Encoding 'cl100k_base'>
+# encoding = tiktoken.encoding_for_model("gpt-4-turbo-preview")
+# print('encoding for gpt-4-turbo-preview:', encoding) # prints <Encoding 'cl100k_base'>
+
+# encoding used by gpt-3.5-turbo and gpt-4-turbo-preview is cl100k_base
+encoding = tiktoken.get_encoding("cl100k_base")
+# sample_tokens = encoding.encode("tiktoken is great!")
+# print(sample_tokens)
+
+# read context 
+context_filepath  = './samples/opencv_live_episode_123/context.txt' 
+with open(context_filepath) as fh : 
+    input_context = fh.read()
+
+# tokenize 
+tokens = encoding.encode(input_context)
+num_tokens = len(tokens)
+print('num_tokens:', num_tokens)
